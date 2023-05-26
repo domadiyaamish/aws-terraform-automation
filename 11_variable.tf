b797bccfa5c89af31bb6900e1bba5792fd05968a
amish <doamdiyaamish@gmail.com>
2023-05-27 00:15:08 +0530

    list varaiavle is added

--- 11_variable.tf
+++ 11_variable.tf
@@ -7,12 +7,18 @@ provider "aws" {
 
 resource "aws_instance" "variable" {
   ami = "ami-02eb7a4783e7e9317"
+  #string 
   instance_type = var.instance_type
+  #number
+  count = var.instance_count
+  #bool
+  associate_public_ip_address = var.asscocation_public_type
   tags = {
     Name = "ec2_variable"
   }
 }
 
+#sting variable
 variable "instance_type" {
   description = "this is my first variable"
   type = string
@@ -21,24 +27,27 @@ variable "instance_type" {
 
 
 # number variable
-provider "aws" {
-  region = "eu-west-1"
-  access_key = ""
-  secret_key = ""
-}
-
-resource "aws_instance" "countinstance" {
-  ami = "ami-02eb7a4783e7e9317"
-  instance_type = "t2.micro"
-  count = var.instance_count
-  tags = {
-    Name = "countinstance"
-  }
-
-}
-
 variable "instance_count" {
   description = "count example"
   type = number
   default = 2
+}
+
+# bool varaible 
+variable "asscocation_public_type" {
+  description = "bool type"
+  type = bool
+  default = true
+}
+
+# list varaible
+resource "aws_iam_user" "exaple" {
+  count = length(var.user_names)
+  name = var.user_names[count.index]
+}
+
+variable "user_names" {
+  description = "list varaible"
+  type = list(string)
+  default = ["user1", "user2", "user3"]
 }
