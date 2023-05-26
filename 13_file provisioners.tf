923c13b686f669c857d52844636ffbe94046b804
amish <doamdiyaamish@gmail.com>
2023-05-27 02:56:00 +0530

    file provision is created

---
+++ 13_file provisioners.tf
@@ -0,0 +1,48 @@
+resource "aws_instance" "ec2_example" {
+
+    ami = "ami-02eb7a4783e7e9317"  
+    instance_type = "t2.micro" 
+    key_name= "aws_key"
+    vpc_security_group_ids = [aws_security_group.main.id]
+
+  provisioner "file" {
+    source      = "/home/rahul/Jhooq/keys/aws/test-file.txt"
+    destination = "/home/ubuntu/test-file.txt"
+  }
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file("/home/rahul/Jhooq/keys/aws/aws_key")
+      timeout     = "4m"
+   }
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
+}
