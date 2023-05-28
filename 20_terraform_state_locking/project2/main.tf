28d7bac251be9cef25ae8675909da2daea22dbc7
amish <doamdiyaamish@gmail.com>
2023-05-29 03:51:58 +0530

    project file is added

---
+++ 20_terraform_state_locking/project2/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = ""
+    access_key = ""
+    secret_key = ""
+}
+
+resource "aws_instance" "ec2_example" {
+  ami = ""
+  instance_type = "t2.micro"
+  tags = {
+    Name = "EC2 Instance with remote state"
+  }
+}
+
+terraform{
+    backend "s3" {
+        bucket = "first-terraform-s3-bucket"
+        key = "terraform/remote/s3/terraform.tfstate"
+        encrypt = true
+        region = "en-west-1"
+        dynamodb_table = "dynamodb-state-locking"
+    }
+}
